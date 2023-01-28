88ef4c27731c8f302a246d173c5cdc539af34f60
victor2905 <98538867+victor2905@users.noreply.github.com>
2023-01-27 22:54:32 -0500

    programa_cadena_caracteres
    Creación de proyecto

---
+++ programa_cadena_caracteres.py
@@ -0,0 +1,37 @@
+mensaje = "Hola"
+mensaje+= " "
+mensaje+= "Victor"
+print(mensaje)
+
+print("Concatenacion")
+
+mensaje1 = "Hola"
+espacio = " "
+nombre = "Victor"
+
+print(mensaje1 + espacio + nombre)
+
+print("Casteo de caracteres")
+
+numero_uno = 2
+numero_dos = 4
+resultado = numero_uno + numero_dos;
+concatenar = str(resultado)
+
+print("El resultado de la suma es:" + concatenar)
+
+print("Busqueda en cadena de caracteres")
+mensaje3 = "Hola estoy aprendiendo Python"
+busqueda_subcadena = mensaje3.find("Python")
+print("La posición es:"+str(busqueda_subcadena))
+
+print("Extracción")
+mensaje4="prueba de extracción"
+extraer_subcadena = mensaje4[10:21]
+print("La subcadena es:"+extraer_subcadena)
+
+print("Comparación de caracteres")
+variable1="Pedro Pedrin"
+variable2="Pedro Pedrin"
+print("resultado comparación: "+ str(variable1 == variable2))
+
